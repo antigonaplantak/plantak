e3ca3ed4fa9fe5008f9a967f0b56cc2a702ac302
antigplantak <antigonavokshi@icloud.com>
2026-02-23 22:47:41 +0100

    test(auth): mock dependencies for unit tests

--- apps/api/src/auth/auth.service.spec.ts
+++ apps/api/src/auth/auth.service.spec.ts
@@ -1,15 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const prismaMock = {
+    user: {
+      findUnique: jest.fn(),
+      create: jest.fn(),
+    },
+    refreshSession: {
+      create: jest.fn(),
+      findUnique: jest.fn(),
+      deleteMany: jest.fn(),
+    },
+  };
+
+  const jwtMock = {
+    signAsync: jest.fn(),
+    verifyAsync: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: PrismaService, useValue: prismaMock },
+        { provide: JwtService, useValue: jwtMock },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    service = module.get(AuthService);
   });
 
   it('should be defined', () => {
